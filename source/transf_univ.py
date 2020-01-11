f87369f093adb16f42c21d0e2c6988b8b7cfc056
lucabasa <luca.basanisi@gmail.com>
2020-01-11 18:15:29 +0100

    notebook 02, new source scripts

---
+++ source/transf_univ.py
@@ -0,0 +1,177 @@
+__author__ = 'lucabasa'
+__version__ = '1.2.3'
+__status__ = 'development'
+
+
+import pandas as pd
+import numpy as np
+
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.pipeline import FeatureUnion
+from sklearn.preprocessing import StandardScaler, RobustScaler
+from sklearn.impute import SimpleImputer
+
+import warnings
+
+
+class feat_sel(BaseEstimator, TransformerMixin):
+    '''
+    This transformer selects either numerical or categorical features.
+    In this way we can build separate pipelines for separate data types.
+    '''
+    def __init__(self, dtype='numeric'):
+        self.dtype = dtype
+ 
+    def fit( self, X, y=None ):
+        return self 
+    
+    def transform(self, X, y=None):
+        if self.dtype == 'numeric':
+            num_cols = X.columns[X.dtypes != object].tolist()
+            return X[num_cols]
+        elif self.dtype == 'category':
+            cat_cols = X.columns[X.dtypes == object].tolist()
+            return X[cat_cols]
+
+
+class df_imputer(TransformerMixin, BaseEstimator):
+    '''
+    Just a wrapper for the SimpleImputer that keeps the dataframe structure
+    '''
+    def __init__(self, strategy='mean'):
+        self.strategy = strategy
+        self.imp = None
+        self.statistics_ = None
+
+    def fit(self, X, y=None):
+        self.imp = SimpleImputer(strategy=self.strategy)
+        self.imp.fit(X)
+        self.statistics_ = pd.Series(self.imp.statistics_, index=X.columns)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Ximp = self.imp.transform(X)
+        Xfilled = pd.DataFrame(Ximp, index=X.index, columns=X.columns)
+        return Xfilled
+
+    
+class df_scaler(TransformerMixin, BaseEstimator):
+    '''
+    Wrapper of StandardScaler or RobustScaler
+    '''
+    def __init__(self, method='standard'):
+        self.scl = None
+        self.scale_ = None
+        self.method = method
+        if self.method == 'sdandard':
+            self.mean_ = None
+        elif method == 'robust':
+            self.center_ = None
+        self.columns = None  # this is useful when it is the last step of a pipeline before the model
+
+    def fit(self, X, y=None):
+        if self.method == 'standard':
+            self.scl = StandardScaler()
+            self.scl.fit(X)
+            self.mean_ = pd.Series(self.scl.mean_, index=X.columns)
+        elif self.method == 'robust':
+            self.scl = RobustScaler()
+            self.scl.fit(X)
+            self.center_ = pd.Series(self.scl.center_, index=X.columns)
+        self.scale_ = pd.Series(self.scl.scale_, index=X.columns)
+        return self
+
+    def transform(self, X):
+        # assumes X is a DataFrame
+        Xscl = self.scl.transform(X)
+        Xscaled = pd.DataFrame(Xscl, index=X.index, columns=X.columns)
+        self.columns = X.columns
+        return Xscaled
+    
+    def get_feature_names(self):
+        return list(self.columns)
+
+
+class dummify(TransformerMixin, BaseEstimator):
+    '''
+    Wrapper for get dummies
+    '''
+    def __init__(self, drop_first=False, match_cols=True):
+        self.drop_first = drop_first
+        self.columns = []  # useful to well behave with FeatureUnion
+        self.match_cols = match_cols
+
+    def fit(self, X, y=None):
+        return self
+    
+    def match_columns(self, X):
+        miss_train = list(set(X.columns) - set(self.columns))
+        miss_test = list(set(self.columns) - set(X.columns))
+        
+        err = 0
+        
+        if len(miss_test) > 0:
+            for col in miss_test:
+                X[col] = 0  # insert a column for the missing dummy
+                err += 1
+        if len(miss_train) > 0:
+            for col in miss_train:
+                del X[col]  # delete the column of the extra dummy
+                err += 1
+                
+        if err > 0:
+            warnings.warn('The dummies in this set do not match the ones in the train set, we corrected the issue.',
+                         UserWarning)
+            
+        return X[self.columns]  # preserve original order to avoid problems with some algorithms
+    
+    def transform(self, X):
+        X = pd.get_dummies(X, drop_first=self.drop_first)
+        if (len(self.columns) > 0):
+            if self.match_cols:
+                X = self.match_columns(X)
+        else:
+            self.columns = X.columns
+        return X
+    
+    def get_features_name(self):
+        return self.columns
+
+ 
+class FeatureUnion_df(TransformerMixin, BaseEstimator):
+    '''
+    Wrapper of FeatureUnion but returning a Dataframe, 
+    the column order follows the concatenation done by FeatureUnion
+
+    transformer_list: list of Pipelines
+
+    '''
+    def __init__(self, transformer_list, n_jobs=None, transformer_weights=None, verbose=False):
+        self.transformer_list = transformer_list
+        self.n_jobs = n_jobs
+        self.transformer_weights = transformer_weights
+        self.verbose = verbose  # these are necessary to work inside of GridSearch or similar
+        self.feat_un = FeatureUnion(self.transformer_list, 
+                                    self.n_jobs, 
+                                    self.transformer_weights, 
+                                    self.verbose)
+        
+    def fit(self, X, y=None):
+        self.feat_un.fit(X, y)
+        return self
+
+    def transform(self, X, y=None):
+        X_tr = self.feat_un.transform(X)
+        columns = []
+        
+        for trsnf in self.transformer_list:
+            cols = trsnf[1].steps[-1][1].get_features_name()
+            columns += list(cols)
+
+        X_tr = pd.DataFrame(X_tr, index=X.index, columns=columns)
+        
+        return X_tr
+
+    def get_params(self, deep=True):  # necessary to well behave in GridSearch
+        return self.feat_un.get_params(deep=deep)
---
+++ source/utility.py
@@ -0,0 +1,105 @@
+__author__ = 'lucabasa'
+__version__ = '1.1.1'
+__status__ = 'development'
+
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import StratifiedShuffleSplit, GridSearchCV, RandomizedSearchCV
+
+import source.report as rp
+
+
+def make_test(train, test_size, random_state, strat_feat=None):
+    '''
+    Creates a train and test, stratified on a feature or on a list of features
+    todo: allow for non-stratified splits
+    '''
+    if strat_feat:
+        
+        split = StratifiedShuffleSplit(n_splits=1, test_size=test_size, random_state=random_state)
+
+        for train_index, test_index in split.split(train, train[strat_feat]):
+            train_set = train.loc[train_index]
+            test_set = train.loc[test_index]
+            
+    return train_set, test_set
+
+
+def cv_score(df_train, y_train, kfolds, pipeline, imp_coef=False):
+    '''
+    Train and test a pipeline in kfold cross validation
+    Returns the oof predictions for the entire train set and a dataframe with the
+    coefficients or feature importances, averaged across the folds, with standard deviation
+
+    todo: report on the average score and variance too
+    '''
+    oof = np.zeros(len(df_train))
+    train = df_train.copy()
+    
+    feat_df = pd.DataFrame()
+    
+    for n_fold, (train_index, test_index) in enumerate(kfolds.split(train.values)):
+            
+        trn_data = train.iloc[train_index][:]
+        val_data = train.iloc[test_index][:]
+        
+        trn_target = y_train.iloc[train_index].values.ravel()
+        val_target = y_train.iloc[test_index].values.ravel()
+        
+        pipeline.fit(trn_data, trn_target)
+
+        oof[test_index] = pipeline.predict(val_data).ravel()
+
+        if imp_coef:
+            try:
+                fold_df = rp.get_coef(pipeline)
+            except AttributeError:
+                fold_df = rp.get_feature_importance(pipeline)
+                
+            fold_df['fold'] = n_fold + 1
+            feat_df = pd.concat([feat_df, fold_df], axis=0)
+       
+    if imp_coef:
+        feat_df = feat_df.groupby('feat')['score'].agg(['mean', 'std'])
+        feat_df['abs_sco'] = (abs(feat_df['mean']))
+        feat_df = feat_df.sort_values(by=['abs_sco'],ascending=False)
+        del feat_df['abs_sco']
+        return oof, feat_df
+    else:    
+        return oof
+
+
+def grid_search(data, target, estimator, param_grid, scoring, cv, random=False):
+    '''
+    Calls a grid or a randomized search over a parameter grid
+    Returns a dataframe with the results for each configuration
+    Returns a dictionary with the best parameters
+    Returns the best (fitted) estimator
+    '''
+    
+    if random:
+        grid = RandomizedSearchCV(estimator=estimator, param_distributions=param_grid, cv=cv, scoring=scoring, 
+                                  n_iter=random, n_jobs=-1, random_state=434, iid=False)
+    else:
+        grid = GridSearchCV(estimator=estimator, param_grid=param_grid, 
+                            cv=cv, scoring=scoring, n_jobs=-1, return_train_score=False)
+    
+    pd.options.mode.chained_assignment = None  # turn on and off a warning of pandas
+    tmp = data.copy()
+    grid = grid.fit(tmp, target)
+    pd.options.mode.chained_assignment = 'warn'
+    
+    result = pd.DataFrame(grid.cv_results_).sort_values(by='mean_test_score', 
+                                                        ascending=False).reset_index()
+    
+    print(result.columns)
+    
+    del result['params']
+    times = [col for col in result.columns if col.endswith('_time')]
+    params = [col for col in result.columns if col.startswith('param_')]
+    
+    result = result[params + ['mean_test_score', 'std_test_score'] + times]
+    
+    return result, grid.best_params_, grid.best_estimator_
