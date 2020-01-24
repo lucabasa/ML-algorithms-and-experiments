aae56dd293c4e9a6286a849fc746f8856aa9b277
lucabasa <luca.basanisi@gmail.com>
2020-01-24 22:56:18 +0100

    better data generated

--- source/generate_data.py
+++ source/generate_data.py
@@ -1,5 +1,5 @@
 __author__ = 'lucabasa'
-__version__ = '1.3.0'
+__version__ = '2.0.0'
 __status__ = 'development'
 
 import pandas as pd
@@ -15,24 +15,27 @@ def make_uncorrelated(data, n_entries):
     np.random.seed(23)
     random.seed(23)
     # normal distributions
-    df['unc_normal_1'] = np.random.normal(loc=0, scale=0.5, size=n_entries)
-    df['unc_normal_2'] = np.random.normal(loc=10, scale=1, size=n_entries)
+    ran_loc = np.random.normal(loc=1, scale=0.8, size=20)
+    ran_scl = np.random.uniform(low=0.01, high=3, size=20)
+    for i in range(1,21):
+        df[f'unc_normal_{i}'] = np.random.normal(loc=ran_loc[i-1], scale=ran_scl[i-1], size=n_entries)
+        
     # Skewed distribution
     df['unc_skewed_pos'] = np.expm1(np.random.normal(loc=1.3, scale=0.5, size=n_entries))
     df['unc_skewed_neg'] = 40 - np.expm1(np.random.normal(loc=2.3, scale=0.3, size=n_entries))
     
     # categorical variables
-    df['unc_binary'] = np.random.choice([0, 1], size=(n_entries,), p=[1/3, 2/3])
-    df['unc_categories_5'] = np.random.choice(['a', 'b', 'c', 'd', 'e'], size=(n_entries,), p=[2/5, 1/5, 1/10, 3/15, 1/10])
-    i = 0
-    random_cats = []
-    # generate 100 random strings of 3 characters
-    while i < 100:
-        random_cats.append(''.join([random.choice(string.ascii_lowercase) for _ in range(3)]))
-        i += 1
-    df['unc_categories_100'] = np.random.choice(random_cats, size=(n_entries,))
+    df['unc_binary_1'] = np.random.choice([0, 1], size=(n_entries,), p=[1/3, 2/3])
+    df['unc_binary_2'] = np.random.choice([0, 1], size=(n_entries,), p=[1/2, 1/2])
+    df['unc_binary_3'] = np.random.choice([0, 1], size=(n_entries,), p=[1/5, 4/5])
+    df['unc_binary_4'] = np.random.choice([0, 1], size=(n_entries,), p=[1/4, 3/4])
+    df['unc_binary_5'] = np.random.choice([0, 1], size=(n_entries,), p=[1/10, 9/10])
     
-    df['unc_ordinal'] = np.random.choice(np.arange(1,100), size=(n_entries,))
+    df['unc_categories_5'] = np.random.choice(['a', 'b', 'c', 'd', 'e'], size=(n_entries,), p=[2/5, 1/5, 1/10, 3/15, 1/10])
+    df['unc_categories_3'] = np.random.choice(['a', 'b', 'c'], size=(n_entries,))
+    
+    for i in range(1,12):
+        df[f'unc_ordinal_{i}'] = np.random.choice(np.arange(1,i*10), size=(n_entries,))
     
     return df
 
@@ -77,46 +80,55 @@ def make_correlated(data, n_entries):
     return df
 
 
-def linear_targets(data, tmp):
+def make_confounding(data):
     df = data.copy()
     np.random.seed(23)
+    random.seed(23)
+    n_entries = data.shape[0]
+    
+    # normal distributions uncorrelated from the rest
+    ran_loc = np.random.normal(loc=0, scale=1, size=40)
+    ran_scl = np.random.uniform(low=0.01, high=5, size=40)
+    for i in range(1,41):
+        df[f'conf_normal_{i}'] = np.random.normal(loc=ran_loc[i-1], scale=ran_scl[i-1], size=n_entries)
+        
+    # interactions of other variables with small noise
+    for i in range(1,6):
+        df[f'conf_inter_{i}'] = df[f'unc_normal_{i}'] * df[f'unc_normal_{i+10}'] + np.random.normal(loc=0, scale=0.2, size=n_entries)
+        
+    df['conf_inter_6'] = df['unc_skewed_pos'] * df['corr_multinormal_high_a'] + np.random.normal(loc=0, scale=0.1, size=n_entries)
+    df['conf_inter_7'] = df['unc_binary_4'] * df['unc_binary_2'] + np.random.normal(loc=1, scale=0.1, size=n_entries)
+    df['conf_inter_8'] = df['unc_binary_1'] * df['unc_binary_2'] + np.random.normal(loc=0.1, scale=0.2, size=n_entries)
+    df['conf_inter_9'] = df['corr_multinormal_mid_b'] * df['corr_multinormal_low_b'] + np.random.normal(loc=-0.1, scale=0.1, size=n_entries)
+    df['conf_inter_10'] = df['corr_multinormal_low_a'] * df['unc_normal_19'] + np.random.normal(loc=-1, scale=0.2, size=n_entries)
+    
+    return df
+
+
+def linear_targets(data, tmp):
+    df = data.copy()
+    np.random.seed(2332)
+    random.seed(234)
     entries = df.shape[0]
     
     coef_dict = {}
     
     # all
-    coefs = [3, 0.4, -0.01, 0.03, -2.4, 0.11, 0.1, 6, 2.36, 7.3, 0.75, 0.69, -9.47, 0.73, 1.98, 4.61,
-       -0.5, 3.8, 0.16, -0.4, -1]
-    df['tar_lin_full'] = tmp.multiply(coefs, axis=1).sum(axis=1) + np.random.normal(0, 11, size=entries)
+    coefs = np.random.normal(loc=0, scale=0.5, size=tmp.shape[1])
+    df['tar_lin_full'] = tmp.multiply(coefs, axis=1).sum(axis=1) + np.random.normal(20, 10, size=entries)
     coef_dict['tar_lin_full'] = pd.DataFrame({'feat': tmp.columns, 'coef': coefs})
     
-    # all the uncorrelated
-    coefs = [3, 0.4, -0.01, 0.03, -2.4, 0.11, 0.1, 6, 2.36, 7.3, 0.75]
-    df['tar_lin_unc'] = (tmp[[col for col in tmp.columns if col.startswith('unc_')]].multiply(coefs, axis=1).sum(axis=1) + 
-                         np.random.normal(1, 0.4, size=entries))
-    coef_dict['tar_lin_unc'] = pd.DataFrame({'feat': [col for col in tmp.columns if col.startswith('unc_')], 'coef': coefs})
+    # with half of them
+    to_use = random.sample([col for col in tmp.columns if 'conf_' not in col], 50)
+    coefs = np.random.normal(loc=0, scale=2, size=len(to_use))
+    df['tar_lin_half'] = tmp[to_use].multiply(coefs, axis=1).sum(axis=1) + np.random.normal(-20, 10, size=entries)
+    coef_dict['tar_lin_half'] = pd.DataFrame({'feat': to_use, 'coef': coefs})
     
-    # all the correlated
-    coefs = [3, 0.4, -0.01, 0.03, -2.4, 0.11, 0.1, -6, 2.36, -0.8]
-    df['tar_lin_corr'] = (tmp[[col for col in tmp.columns if col.startswith('corr_')]].multiply(coefs, axis=1).sum(axis=1) + 
-                          np.random.normal(4, 3, size=entries))
-    coef_dict['tar_lin_corr'] = pd.DataFrame({'feat': [col for col in tmp.columns if col.startswith('corr_')], 'coef': coefs})
-    
-    # 3 variables
-    coefs = [0.4, -0.01, 0.03]
-    df['tar_lin_3'] = (tmp[['unc_normal_1', 'corr_normal_by_cat', 'corr_multinormal_mid_a']].multiply(coefs, axis=1).sum(axis=1) + 
-                       np.random.normal(-0.7, 1, size=entries))
-    coef_dict['tar_lin_3'] = pd.DataFrame({'feat': ['unc_normal_1', 'corr_normal_by_cat', 'corr_multinormal_mid_a'], 'coef': coefs})
-    
-    # 3 variables and interactions
-    coefs = [0.4, -0.01, 0.03]
-    df['tar_lin_3int'] = (tmp[['unc_normal_1', 'corr_normal_by_cat', 'corr_multinormal_mid_a']].multiply(coefs, axis=1).sum(axis=1) + 
-                          0.6 * tmp['unc_normal_1'] * tmp['corr_normal_by_cat'] -
-                          1.3 * tmp['corr_normal_by_cat'] * tmp['unc_normal_1'] * tmp['corr_normal_by_cat'] + 
-                          np.random.normal(-0.7, 1, size=entries))
-    coef_dict['tar_lin_3int'] = pd.DataFrame({'feat': ['unc_normal_1', 'corr_normal_by_cat', 'corr_multinormal_mid_a', 
-                                                       'unc_normal_1__corr_normal_by_cat', 'all_3'], 
-                                              'coef': coefs + [0.6, 1.3]})
+    # with 10 of them
+    to_use = random.sample([col for col in tmp.columns if 'conf_' not in col and 'categories' not in col], 10)
+    coefs = np.random.normal(loc=0, scale=0.5, size=len(to_use))
+    df['tar_lin_10'] = tmp[to_use].multiply(coefs, axis=1).sum(axis=1) + np.random.normal(0, 1, size=entries)
+    coef_dict['tar_lin_10'] = pd.DataFrame({'feat': to_use, 'coef': coefs})
     
     return df, coef_dict
 
@@ -128,47 +140,28 @@ def nonlinear_targets(data, tmp):
     
     coef_dict = {}
     
-    # all
-    coefs = [3, 0.4, 0.01, 0.03, 2.4, 0.11, 0.1, 6, 2.36, 7.3, 0.75, 0.69, 9.47, 0.73, 1.98, 4.61,
-       0.5, 3.8, 0.16, 0.4, 1]
-    df['tar_nonlin_full'] = 0.5*np.expm1(-tmp.multiply(coefs, axis=1).sum(axis=1) / 100) + np.random.normal(2, 2, size=entries)
-    coef_dict['tar_nonlin_full'] = pd.DataFrame({'feat': tmp.columns, 'coef': coefs})
-    
-    # all the uncorrelated
-    coefs = [3, 0.4, -0.01, 0.03, -2.4, 0.11, 0.1, 6, 2.36, 7.3, 0.75]
-    df['tar_nonlin_unc'] = (np.expm1(tmp[[col for col in tmp.columns 
-                                         if col.startswith('unc_')]].multiply(coefs, axis=1).sum(axis=1) / 20) + 
-                            np.random.normal(0, 10, size=entries))
-    coef_dict['tar_nonlin_unc'] = pd.DataFrame({'feat': [col for col in tmp.columns if col.startswith('unc_')], 'coef': coefs})
-    
-    # all the correlated
-    coefs = [3, 0.4, -0.01, 0.03, -2.4, 0.11, 0.1, -6, 2.36, -0.8]
-    df['tar_nonlin_corr'] = (np.expm1(tmp[[col for col in tmp.columns 
-                                          if col.startswith('corr_')]].multiply(coefs, axis=1).sum(axis=1) / 20) + 
-                             np.random.normal(4, 2, size=entries))
-    coef_dict['tar_nonlin_corr'] = pd.DataFrame({'feat': [col for col in tmp.columns if col.startswith('corr_')], 'coef': coefs})
-    
-    # 3 variables
-    df['tar_nonlin_3'] = (0.04*tmp['unc_ordinal']**2 - 0.09*tmp['corr_normal_by_cat'] + 0.8*np.tanh(tmp['corr_multinormal_mid_a']) + 
-                          np.random.normal(-0.7, 10, size=entries))
-    coef_dict['tar_nonlin_3'] = pd.DataFrame({'feat': ['unc_normal_1_squared', 'corr_normal_by_cat', 'corr_multinormal_mid_a_tanh'], 
-                                           'coef': [0.04, -0.09, 0.8]})
-    
-    # 3 variables and interactions
-    df['tar_nonlin_3int'] = (0.04*tmp['unc_ordinal']**2 - 0.09*tmp['corr_normal_by_cat'] - 0.8*np.tanh(tmp['corr_multinormal_mid_a']) + 
-                          0.6 * tmp['unc_normal_1'] * tmp['corr_normal_by_cat'] -
-                          0.3 * tmp['corr_normal_by_cat'] * tmp['unc_normal_1'] * tmp['corr_normal_by_cat'] + 
-                          np.random.normal(-0.7, 10, size=entries))
-    coef_dict['tar_nonlin_3int'] = pd.DataFrame({'feat': ['unc_normal_1_squared', 'corr_normal_by_cat', 'corr_multinormal_mid_a_tanh', 
-                                                          'unc_normal_1__corr_normal_by_cat', 'all_3'], 
-                                           'coef': [0.04, -0.09, 0.8, 0.6, -0.3]})
+    df['tar_nonlin_10'] = -200 + (0.3 * df['unc_normal_1']**2 
+                           -0.7 * df['unc_normal_7']**2 
+                           -1 * df['unc_skewed_pos'] * df['unc_skewed_neg'] 
+                           +0.8 * df['corr_multinormal_mid_a']**2 
+                           +0.5 * df['unc_ordinal_5'] * df['unc_normal_12'] 
+                           -0.4 * df['unc_normal_14'] * df['corr_normal_by_cat'] 
+                           + df['unc_normal_1'] * df['unc_normal_7']
+                          )
+    coef_dict['tar_nonlin_10'] = pd.DataFrame({'feat': ['unc_normal_1_sq', 
+                                                        'unc_normal_7_sq', 
+                                                        'unc_skewed_int', 
+                                                        'corr_multinormal_mid_a_sq', 
+                                                        'unc_ordinal_5_12', 
+                                                        'unc_normal_14_by_cat', 
+                                                        'unc_normal_1_7'],
+                                               'coef': [0.3, -0.7, -1, 0.8, 0.5, -0.4, 1]})
     
     return df, coef_dict
 
 
 def make_targets(data):
     tmp = data.copy()
-    tmp['unc_categories_100'] = tmp.unc_categories_100.astype('category').cat.codes.astype(int)
     tmp = pd.get_dummies(tmp, drop_first=True)
     
     df, coef_lin = linear_targets(data, tmp)
@@ -178,10 +171,9 @@ def make_targets(data):
 
 
 def dirtify(data):
-    # do not add missing values to the targets
-    df = data[[col for col in data.columns if 'tar_' not in col]].copy()
+    df = data.copy()
     n_entries = df.shape[0]
-    np.random.seed(23)
+    np.random.seed(11)
     
     # adding random noise to some columns
     df['unc_normal_1'] += np.random.normal(loc=0, scale=15, size=n_entries) / 10
@@ -194,13 +186,6 @@ def dirtify(data):
     df['unc_normal_2'] = df['unc_normal_2'].mask(random_entries).fillna(df['unc_normal_2'] + 20)
     random_entries = np.random.random(df.shape[0])<0.00001
     df['corr_multinormal_mid_a'] = df['corr_multinormal_mid_a'].mask(random_entries).fillna(df['corr_multinormal_mid_a'] - 54)
-    
-    # adding 0.5% missing values
-    missing = np.random.random(df.shape)<0.005
-    df = df.mask(missing)
-    
-    # putting the targets back
-    df = df.join(data[[col for col in data.columns if 'tar_' in col]])
     
     return df
 
